1c32bc94dcdeeebe83d56b6196b91dce7a2e7c32
Max Katz-Christy <maxkatzchristy@gmail.com>
2024-10-31 02:38:05 -0400

    updates

---
+++ assets/js/custom.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([41.8682253,-87.6750155], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/maxtkc/doe-mcats
+
+go 1.22.2
+
+require (
+	github.com/FortAwesome/Font-Awesome v0.0.0-20210316185733-d79d85c3fad8 // indirect
+	github.com/hugo-mods/icons v0.0.0-20220508080705-d938a1900b1c // indirect
+)
